2bcaf45b3b529c63b88d29aeec84dfe7b5bfccb9
mireille <ngbtransport2000@gmail.com>
2025-09-13 21:25:28 -0400

    🔧 Adaptation du port pour Render

--- mkbeauty-backend/server.js
+++ mkbeauty-backend/server.js
@@ -215,7 +215,7 @@ app.get("/", (req, res) => {
 
  const PORT = process.env.PORT || 3000;
 
- app.listen(PORT, () => {
-   console.log(`✅ Serveur lancé sur http://localhost:${PORT}`);
+ app.listen(PORT, "0.0.0.0", () => {
+   console.log(`✅ Serveur lancé sur le port ${PORT}`);
  });
  
